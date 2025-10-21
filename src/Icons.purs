f3b4f6a1b5862508b8e6efcb056056f22e750849
Joe Warren <hungryjoe.warren@gmail.com>
2025-10-21 19:41:58 +0100

    move colours into css

--- src/Icons.purs
+++ src/Icons.purs
@@ -13,13 +13,11 @@ iconFlag attrs =
   ( attrs <> [ ])
   [ elementNS ns (ElemName "path")
     [ attr (AttrName "class") "flagpole"
-    , attr (AttrName "fill") "black"
     , attr (AttrName "d") "M 10 30 L 18 30 L 18 28 L 15 26 L 15 2 L 13 2 L 13 26 L 10 28 z"
     ]
     [ 
     ], elementNS ns (ElemName "path")
     [ attr (AttrName "class") "flag"
-    , attr (AttrName "fill") "red"
     , attr (AttrName "d") "M 13 2 L 15 2 L 28 6 L 15 10 L 13 10 z"
     ]
     [ 
@@ -32,13 +30,11 @@ iconUnclicked attrs =
   ( attrs <> [ ])
   [ elementNS ns (ElemName "path")
     [ attr (AttrName "class") "light"
-    , attr (AttrName "fill") "white"
     , attr (AttrName "d") "M 32 0 L 30 2 L 2 2 L 2 30 L 0 32 L 0 0 z"
     ]
     [ 
     ], elementNS ns (ElemName "path")
     [ attr (AttrName "class") "shadow"
-    , attr (AttrName "fill") "darkgrey"
     , attr (AttrName "d") "M 32 0 L 30 2 L 30 30 L 2 30 L 0 32 L 32 32z"
     ]
     [ 
